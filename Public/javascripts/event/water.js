ec462838165e7654989b68a647961dfa12db6fd5
zjhsdtc <zjhsdtc@gmail.com>
2014-03-14 21:39:25 +0800

    comment join count in event index page

--- Public/javascripts/event/water.js
+++ Public/javascripts/event/water.js
@@ -46,7 +46,9 @@
 				len = datas.length;
 			if (len > 0) {
 				$.each(datas, function(i,item){
-					var html = '<div class="title"><a href="/event/' + item.id + '">'+ item.title +'</a></div><div class="intro">时间：'+ item.time + '<br />地点：' + item.location +'</div><div class="pa">'+ item.join_count +'人参加<span class="sp">|</span>'+ item.interest_count +'人感兴趣</div>';
+					// var html = '<div class="title"><a href="/event/' + item.id + '">'+ item.title +'</a></div><div class="intro">时间：'+ item.time + '<br />地点：' + item.location +'</div><div class="pa">'+ item.join_count +'人参加<span class="sp">|</span>'+ item.interest_count +'人感兴趣</div>';
+					var html = '<div class="title"><a href="/event/' + item.id + '">'+ item.title +'</a></div><div class="intro">时间：'+ item.time + '<br />地点：' + item.location +'</div><div class="pa">'+ item.interest_count +'人感兴趣</div>';
+
 					if(item.poster.length>0){
 						var pic = '<div class="pic"><a href="/event/' + item.id + '"><img src="'+ item.poster +'" /></a></div>';
 						html = pic + html;
