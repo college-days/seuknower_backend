e41ecf4e8273eaa846630165abeecaf9462bc24d
zjhsdtc <zjhsdtc@gmail.com>
2014-05-07 12:47:21 +0800

    a little try to finish the function that can auto share commodity when some finish create a commodity

--- Public/javascripts/market/market_detail.js
+++ Public/javascripts/market/market_detail.js
@@ -1,4 +1,9 @@
 var atUserId = 0;
+
+$(function(){
+	//$("#rr_share").click();
+});
+
 $(function(){
     $(".reply").click(function(){
         var href = $(this).attr("href");
@@ -137,6 +142,17 @@ $(function(){
 		showDelete();
 	});
 });
+
+function shareClick() {
+	var rrShareParam = {
+		resourceUrl : '',	//分享的资源Url
+		srcUrl : '',	//分享的资源来源Url,默认为header中的Referer,如果分享失败可以调整此值为resourceUrl试试
+		pic : '',		//分享的主题图片Url
+		title : '',		//分享的标题
+		description : ''	//分享的详细描述
+	};
+	rrShareOnclick(rrShareParam);
+}
 
 function submitComment(){
 	$("div.alert").hide();
